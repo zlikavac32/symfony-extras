def0510d8f9281fc19a8dc7a3bfe26ec31f94988
Marijan Šuflaj <msufflaj32@gmail.com>
2019-03-19 16:24:06 +0100

    Refactor DecoratorPass internals
    Flow is now easier to follow, with steps now being better
    separated

--- src/DependencyInjection/Compiler/DecoratorPass.php
+++ src/DependencyInjection/Compiler/DecoratorPass.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Zlikavac32\SymfonyExtras\DependencyInjection\Compiler;
 
+use Ds\Hashable;
 use Ds\Map;
 use Ds\Sequence;
 use Ds\Set;
@@ -12,7 +13,6 @@ use LogicException;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 use function Zlikavac32\SymfonyExtras\DependencyInjection\assertDefinitionIsAbstract;
 use function Zlikavac32\SymfonyExtras\DependencyInjection\assertOnlyOneTagPerService;
@@ -43,6 +43,10 @@ class DecoratorPass implements CompilerPassInterface
      * @var Map|DecoratorDefinition[]
      */
     private $decoratorDefinitions;
+    /**
+     * @var Set|TagReference[]
+     */
+    private $processedTags;
 
     public function __construct(string $tag = 'decorator')
     {
@@ -70,12 +74,23 @@ class DecoratorPass implements CompilerPassInterface
 
             $this->decoratorDefinitions = $container->get($decoratorDefinitions);
 
+            $processedTags = self::class . '.processed_tags';
+
+            if (!$container->has($processedTags)) {
+                $container->set($processedTags, new Set());
+            }
+
+            $this->processedTags = $container->get($processedTags);
+
             $this->buildMapOfDecorators($container);
 
-            $this->decorateServices($container);
+            $tagsToProcess = $this->resolveTagsToProcess($container);
+
+            $this->decorateServices($container, $tagsToProcess);
         } finally {
             $this->tagToServicesMap = null;
             $this->decoratorDefinitions = null;
+            $this->processedTags = null;
         }
     }
 
@@ -129,109 +144,99 @@ class DecoratorPass implements CompilerPassInterface
         }
     }
 
-    private function decorateServices(ContainerBuilder $container): void
+    /**
+     * @param ContainerBuilder $container
+     *
+     * @return Map|Vector[]
+     */
+    private function resolveTagsToProcess(ContainerBuilder $container): Map
     {
-        $usedDecorators = $this->decoratorDefinitions->filter(function (string $tagName): bool {
-            return $this->tagToServicesMap->hasKey($tagName);
-        });
+        $servicesToProcess = new Set();
 
-        foreach ($usedDecorators as $tagName => $decoratorDefinition) {
-            assert($decoratorDefinition instanceof DecoratorDefinition);
-
-            foreach ($this->tagToServicesMap->get($tagName)
-                         ->diff($decoratorDefinition->processedServices()) as $serviceToProcess) {
-
-                $this->decorateService(
-                    $container,
-                    $container->findDefinition($serviceToProcess),
-                    $serviceToProcess
-                );
+        foreach ($this->decoratorDefinitions->keys() as $tagName) {
+            if ($this->tagToServicesMap->hasKey($tagName)) {
+                $servicesToProcess = $servicesToProcess->merge($this->tagToServicesMap->get($tagName));
             }
         }
-    }
 
-    private function decorateService(ContainerBuilder $container, Definition $definition, string $serviceId): void
-    {
-        $serviceTags = new Vector();
+        $tagsToProcess = new Map();
 
-        foreach ($definition->getTags() as $serviceTagName => $tags) {
-            $serviceTags->push([$serviceTagName, $tags]);
+        foreach ($servicesToProcess as $serviceId) {
+            $tagsToProcess->put($serviceId, $this->resolveTagsToProcessFromService($container, $serviceId));
         }
 
-        $servicesToDecorate = $serviceTags
+        return $tagsToProcess->filter(function (string $serviceId, Sequence $tagReferences): bool {
+            return $tagReferences->isEmpty() === false;
+        });
+    }
+
+    private function resolveTagsToProcessFromService(ContainerBuilder $container, string $serviceId): Sequence
+    {
+        $tags = new Vector();
+
+        foreach ($container->findDefinition($serviceId)
+                     ->getTags() as $serviceTagName => $serviceTags) {
+            $tags->push([$serviceTagName, $serviceTags]);
+        }
+
+        return $tags
             ->filter(function (array $tags): bool {
                 return $this->decoratorDefinitions->hasKey($tags[0]);
             })
-            ->map(function (array $tags) use ($serviceId): array {
+            ->map(function (array $tags) use ($serviceId): TagReference {
                 assertOnlyOneTagPerService($tags[1], $tags[0], $serviceId);
 
-                return [$tags[0], $tags[1][0]];
-            })
-            ->map(function (array $tags): array {
-                return [$tags[0], $tags[1], $this->decoratorDefinitions->get($tags[0])];
-            });
+                $tag = $tags[1][0];
 
-        $this->decorateServiceDefinition($container, $servicesToDecorate, $serviceId);
+                $decoratingPriority = $tag['priority'] ?? 0;
+
+                assertValueIsOfType($decoratingPriority, new Set(['integer']), 'priority', $serviceId);
+
+                unset($tag['priority']);
+
+                return new TagReference($tags[0], $serviceId, $decoratingPriority, $tag);
+            })
+            ->filter(function (TagReference $tagReference): bool {
+                return !$this->processedTags->contains($tagReference);
+            });
     }
 
-    private function decorateServiceDefinition(
-        ContainerBuilder $container,
-        Sequence $servicesToDecorate,
-        string $serviceId
-    ): void {
-        foreach ($servicesToDecorate as [$serviceTagName, $tag, $decoratorDefinition]) {
-            assert($decoratorDefinition instanceof DecoratorDefinition);
+    private function decorateServices(ContainerBuilder $container, Map $tagsToProcess): void
+    {
+        foreach ($tagsToProcess as $serviceId => $tagReferences) {
+            foreach ($tagReferences as $tagReference) {
+                assert($tagReference instanceof TagReference);
 
-            if ($decoratorDefinition->processedServices()
-                ->contains($serviceId)) {
-                continue;
+                $this->decorateServiceForTag($container, $this->decoratorDefinitions->get($tagReference->tagName()),
+                    $tagReference);
+
+                $this->processedTags->add($tagReference);
             }
-
-            $decoratingPriority = $tag['priority'] ?? 0;
-
-            assertValueIsOfType($decoratingPriority, new Set(['integer']), 'priority', $serviceId);
-
-            $this->decorateServiceForTag(
-                $container,
-                $serviceTagName,
-                $decoratorDefinition->serviceId(),
-                $decoratorDefinition->argument(),
-                $serviceId,
-                $decoratingPriority,
-                $tag
-            );
-
-            $decoratorDefinition->processedServices()
-                ->add($serviceId);
         }
     }
 
     private function decorateServiceForTag(
         ContainerBuilder $container,
-        string $tagName,
-        string $templateServiceId,
-        $argument,
-        string $serviceId,
-        int $decoratingPriority,
-        array $tag
+        DecoratorDefinition $decoratorDefinition,
+        TagReference $tagReference
     ): void {
-        $decoratingServiceId = sprintf('%s.%s', $serviceId, $tagName);
+        $decoratingServiceId = sprintf('%s.%s', $tagReference->serviceId(), $tagReference->tagName());
 
-        $decoratingService = new ChildDefinition($templateServiceId);
+        $decoratingService = new ChildDefinition($decoratorDefinition->serviceId());
 
-        $newTags = $container->findDefinition($templateServiceId)
+        $newTags = $container->findDefinition($decoratorDefinition->serviceId())
             ->getTags();
 
         unset($newTags[$this->tag]);
 
         $decoratingService
-            ->setArgument($argument, new Reference(sprintf('%s.inner', $decoratingServiceId)))
-            ->setDecoratedService($serviceId, null, $decoratingPriority)
+            ->setArgument($decoratorDefinition->argument(), new Reference(sprintf('%s.inner', $decoratingServiceId)))
+            ->setDecoratedService($tagReference->serviceId(), null, $tagReference->priority())
             ->setTags(
                 $newTags
             );
 
-        foreach (reconstructTags($tag) as $newTagName => $newTagDefinitions) {
+        foreach (reconstructTags($tagReference->remainingProperties()) as $newTagName => $newTagDefinitions) {
             foreach ($newTagDefinitions as $newTagDefinition) {
                 $decoratingService->addTag($newTagName, $newTagDefinition);
             }
@@ -244,23 +249,98 @@ class DecoratorPass implements CompilerPassInterface
 /**
  * @internal
  */
-class DecoratorDefinition {
+class TagReference implements Hashable
+{
+
+    /**
+     * @var string
+     */
+    private $tagName;
+    /**
+     * @var string
+     */
+    private $serviceId;
+    /**
+     * @var int
+     */
+    private $priority;
+    /**
+     * @var string
+     */
+    private $hash;
+    /**
+     * @var array
+     */
+    private $remainingProperties;
+
+    public function __construct(string $tagName, string $serviceId, int $priority, array $remainingProperties)
+    {
+        $this->tagName = $tagName;
+        $this->serviceId = $serviceId;
+        $this->priority = $priority;
+        $this->hash = sha1($tagName . ':' . $serviceId);
+        $this->remainingProperties = $remainingProperties;
+    }
+
+    public function remainingProperties(): array
+    {
+        return $this->remainingProperties;
+    }
+
+    public function tagName(): string
+    {
+        return $this->tagName;
+    }
+
+    public function serviceId(): string
+    {
+        return $this->serviceId;
+    }
+
+    public function priority(): int
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    function hash(): string
+    {
+        return $this->hash;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    function equals($obj): bool
+    {
+        if (!$obj instanceof self) {
+            return false;
+        }
+
+        return $obj->tagName === $this->tagName
+            &&
+            $obj->serviceId === $this->serviceId;
+    }
+}
+
+/**
+ * @internal
+ */
+class DecoratorDefinition
+{
 
     /**
      * @var string
      */
     private $serviceId;
     private $argument;
-    /**
-     * @var Set
-     */
-    private $processedServices;
 
     public function __construct(string $serviceId, $argument)
     {
         $this->serviceId = $serviceId;
         $this->argument = $argument;
-        $this->processedServices = new Set();
     }
 
     public function serviceId(): string
@@ -271,10 +351,5 @@ class DecoratorDefinition {
     public function argument()
     {
         return $this->argument;
-    }
-
-    public function processedServices(): Set
-    {
-        return $this->processedServices;
     }
 }
